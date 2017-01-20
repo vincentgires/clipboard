ce1ad4dcddd53c081723c0f402109dcb98893ab8
Vincent Girès <vincent.gires@gmail.com>
2017-01-20 13:28:15 +0100

    systray menu

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-import sys, os
-from PyQt5 import QtWidgets, QtGui
+#!/usr/bin/env python3
 
+import sys, os
+from PyQt5 import QtWidgets, QtGui, QtCore
+
+
+class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
+    def __init__(self, parent=None):
+        super().__init__()
+        self._parent = parent
+        
+        self.setIcon(self._parent.icon)
+        self.setVisible(True)
+        self.activated.connect(self.show_window)
+        
+        menu = QtWidgets.QMenu(parent)
+        show_action = menu.addAction("Show")
+        show_action.triggered.connect(self.show_window)
+        hide_action = menu.addAction("Hide")
+        hide_action.triggered.connect(self.hide_window)
+        exit_action = menu.addAction("Exit")
+        exit_action.triggered.connect(self.exit)
+        self.setContextMenu(menu)
+        
+    def exit(self):
+        sys.exit()
+        
+    def show_window(self):
+        self._parent.show()
+        
+    def hide_window(self):
+        self._parent.hide()
+        
 
 class ClipbordEdit(QtWidgets.QTextEdit):
     
@@ -40,8 +70,12 @@ class MainWindow(QtWidgets.QWidget):
         grid.addWidget(normpath_btn)
         grid.addWidget(to_slashes_btn)
         grid.addWidget(to_backslashes_btn)
-        
         self.setLayout(grid) 
+        
+        filedir = os.path.dirname(__file__)
+        iconpath = os.path.join(filedir, 'icons', 'accessories-text-editor.svg')
+        self.icon = QtGui.QIcon(iconpath)
+        self.setWindowIcon(self.icon)
         
         self.resize(300, 200)
         self.setWindowTitle('Clipboard')    
@@ -50,17 +84,12 @@ class MainWindow(QtWidgets.QWidget):
     
     
     def createSysTray(self):
-        self.sysTray = QtWidgets.QSystemTrayIcon(self)
-        dir = os.path.dirname(__file__)
-        filepath = os.path.join(dir, 'icons', 'accessories-text-editor.png')
-        self.sysTray.setIcon(QtGui.QIcon(filepath))
-        self.sysTray.setVisible(True)
-        #self.connect(self.sysTray, QtCore.SIGNAL("activated(QSystemTrayIcon::ActivationReason)"), self.on_sys_tray_activated)
-
-        #self.sysTrayMenu = QtGui.QMenu(self)
-        #act = self.sysTrayMenu.addAction("FOO")
+        self.sysTray = SystemTrayIcon(self)
     
-    
+    def closeEvent(self, event):
+        self.hide()
+        event.ignore()
+        
     def copyClicked(self):
         text = self.clipbord_edit.toPlainText()
         self.clipboard.setText(text)
@@ -84,6 +113,10 @@ class MainWindow(QtWidgets.QWidget):
         text = self.clipboard.text()
         self.clipbord_edit.setText(text)
         
+
+
+
+
 if __name__ == '__main__':
     
     app = QtWidgets.QApplication(sys.argv)
